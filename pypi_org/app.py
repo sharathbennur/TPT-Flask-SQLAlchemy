4c33ac27bb30d7f3b732b3ef1cd19dabb12959c6
sharathbennur <sharath.bennur@gmail.com>
2020-03-02 22:36:44 -0500

    added for loops in the templates for packages and basic html

--- pypi_org/app.py
+++ pypi_org/app.py
@@ -3,9 +3,18 @@ import flask
 app = flask.Flask('pypi')
 
 
+def get_test_packages():
+    return [
+        {'name': 'flask', 'version': '1.1.3'},
+        {'name': 'sqlalchemy', 'version': '2.2.0'},
+        {'name': 'passlib', 'version': '1.0.0'},
+    ]
+
+
 @app.route('/')
 def index():
-    return flask.render_template('index.html')
+    test_packages = get_test_packages()
+    return flask.render_template('index.html', packages=test_packages)
 
 
 if __name__ == '__main__':
