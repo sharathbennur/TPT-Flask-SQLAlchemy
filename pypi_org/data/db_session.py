d40bd210fda142090f2e2c665a0cecc61668c9b6
Sharath Bennur <sharath.bennur@gmail.com>
2020-04-07 21:58:29 -0400

    added data import script from the course, fixed path for running the script

--- pypi_org/data/db_session.py
+++ pypi_org/data/db_session.py
@@ -8,9 +8,9 @@ __factory = None
 
 
 def global_init(db_file: str):
-    global factory
+    global __factory
 
-    if factory:
+    if __factory:
         return
 
     if not db_file or not db_file.strip():
@@ -20,7 +20,7 @@ def global_init(db_file: str):
     print("Connecting to DB with {}".format(conn_str))
 
     engine = sa.create_engine(conn_str, echo=False)
-    factory = orm.sessionmaker(bind=engine)
+    __factory = orm.sessionmaker(bind=engine)
 
     # noinspection PyUnresolvedReferences
     import pypi_org.data.__all_models
